e0bbce0284829f757e7c20ce4352317dfc2f5d4a
James Thewlis <jamt9000@gmail.com>
2012-12-12 10:32:36 +0000

    Start of Harris corner detection

--- webcv-shaders.js
+++ webcv-shaders.js
@@ -184,8 +184,71 @@
                 "    vec4 colour = texture2D(uSampler, vTextureCoord);",
                 "    gl_FragColor = vec4( colour.r/res,colour.gba);",
                 "}"
-]
-
+            ]
+        },
+        harris: {
+            vertex: [
+                "uniform vec2 uResolution;",
+                "attribute vec2 aPosition;",
+                "attribute vec2 aTextureCoord;",
+                "varying vec2 vTextureCoord;",
+                "void main () {",
+                "   vec2 normCoords = ((aPosition/uResolution) * 2.0) -1.0;",
+                "   gl_Position = vec4(normCoords, 0, 1);",
+                "   vTextureCoord = aTextureCoord;",
+                "}"
+            ],
+            fragment: [
+                "precision mediump float;",
+                "uniform sampler2D uSampler;",
+                "uniform mat3 uKernel;",
+                "uniform vec2 uImageSize; // for pixel based calculation",
+                "varying vec2 vTextureCoord; // from vertex shader",
+                "void main() {",
+                "    // to convert to pixel units",
+                "    vec2 px = vec2(1.0, 1.0) / uImageSize;",
+                "    float thresh = 0.15;",
+                "    float neighbourSum = 0.0;",
+                "    mat3 sobelX = mat3(-1,-2,-1,0,0,0,1,2,1);",
+                "    mat3 sobelY = mat3(-1,0,1,-2,0,2,-1,0,1);",
+                "    for(int c=0; c<3; c++){",
+                "        for(int r=0; r<3; r++){",
+                "          vec2 offs = vec2(c-1,r-1) * px;",
+                "          float kernelVal = sobelX[c][r];",
+                "          vec4 colour = texture2D(uSampler, vTextureCoord + offs);",
+                "          float grey = 0.299 * colour.r + 0.587 * colour.g + 0.114 * colour.b;",
+                "          neighbourSum += grey * kernelVal;",
+                "        }",
+                "    }",
+                "    float valueX = neighbourSum;",
+                "    for(int c=0; c<3; c++){",
+                "        for(int r=0; r<3; r++){",
+                "          vec2 offs = vec2(c-1,r-1) * px;",
+                "          float kernelVal = sobelY[c][r];",
+                "          vec4 colour = texture2D(uSampler, vTextureCoord + offs);",
+                "          float grey = 0.299 * colour.r + 0.587 * colour.g + 0.114 * colour.b;",
+                "          neighbourSum += grey * kernelVal;",
+                "        }",
+                "    }",
+                "    float valueY = neighbourSum;",
+                "    float Ixx = valueX * valueX;",
+                "    float Iyy = valueY * valueY;",
+                "    float Ixy = valueX * valueY;",
+                "    // Determinant and trace of the harris matrix",
+                "    float det = Ixx * Iyy - Ixy * Ixy;",
+                "    float trace = Ixx + Iyy;",
+                "    // Reflect large eigenvalues",
+                "    float res = 100000000.0 * det / trace;",
+                "    if(res > 5.0){",
+                "       gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);",
+                "    } else if(res > 1.0){",
+                "       gl_FragColor = vec4(0.0, 0.2 * res, 1.0, 1.0);",
+                "    }",
+                "    else{",
+                "       gl_FragColor = vec4(0.0, 0.0, res, 1.0);",
+                "    }",
+                "}"
+            ]
         }
     };
 
